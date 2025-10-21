8b26efac6c334d6fb9b6a2ea6771780a3d1e13f3
Erik van der Bas <erik@basedonline.nl>
2025-10-21 12:35:39 +0200

    fix: update PHP and Rector version requirements; remove unused Rector sets

--- rector.php
+++ rector.php
@@ -3,11 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector;
-use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
-use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 
 return RectorConfig::configure()
@@ -30,8 +25,6 @@ return RectorConfig::configure()
    )
    ->withSkip([
       // Skip rules that might be too aggressive for WordPress projects
-      StaticClosureRector::class,
-      StaticArrowFunctionRector::class,
       ReadOnlyPropertyRector::class,
    ])
    ->withRules([
